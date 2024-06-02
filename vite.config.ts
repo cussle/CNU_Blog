b494d9f0491d549f8c4b51d42a1e3aa652269d10
cussle <cussle@kakao.com>
2024-06-02 21:35:45 +0900

    edit: config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/CNU_Blog/',
+  base: process.env.NODE_ENV === 'production' ? '/CNU_Blog/' : '/',
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    },
+  },
 });
